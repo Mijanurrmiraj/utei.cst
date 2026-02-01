108dc6092770383a1cfd66047c4318c8409492d9
Mijanur Rahman Miraj <mirajtv11@gmail.com>
2026-02-02 00:18:39 +0600

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <section className="text-center py-20">
+      <h1 className="text-4xl font-bold text-green-700">
+        Welcome to Computer Department
+      </h1>
+      <p className="mt-4">
+        Upashahar Textile Engineering Institute, Dinajpur
+      </p>
+    </section>
+  );
+}
